d4295ed9e130e86951e51800c69621907c8aca40
Beth Smith <bethsmith0623@gamil.com>
2019-08-15 10:31:40 -0500

    finishes landing page

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const flightsRouter = require('./routes/flights');
+const indexRouter = require('./routes/index');
 const port = 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
 
 app.listen(port, () => {
