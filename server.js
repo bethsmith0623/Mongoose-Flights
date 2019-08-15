7daf14309a6b3eda098fef8e46100ec664b02956
Beth Smith <bethsmith0623@gamil.com>
2019-08-15 10:11:52 -0500

    adds view engine

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const flightsRouter = require('./routes/flights');
 const port = 3000;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/flights', flightsRouter);
 
 app.listen(port, () => {
   console.log(`Express is listening on port ${port}`);
