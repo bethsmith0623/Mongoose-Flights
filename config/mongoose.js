a6b166abe73b84da41548b32ffe42da762bddc92
Beth Smith <bethsmith0623@gamil.com>
2019-08-16 10:08:57 -0500

    updates config

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb//localhost:27017/mongooseFlights', {
+mongoose.connect('mongodb://localhost:27017/mongooseFlights', {
   useNewUrlParser: true,
   useCreateIndex: true
 });
-
 
 const db = mongoose.connection;
 
