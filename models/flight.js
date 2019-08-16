94e0d1bb7f4dd25863a24b1b57e2985427da4442
Beth Smith <bethsmith0623@gamil.com>
2019-08-16 10:26:10 -0500

    sets up flight Schema

---
+++ models/flight.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  flightNo: {
+    type: Number,
+    required: true,
+    min: 10,
+    max: 9999
+  },
+  departs: {
+    type: Date, 
+    default: function(){
+      // 08/16/2019 22:20 CDT convert to 08/19/2020 22:20 CDT
+        const date = new Date();
+        const updatedYear = date.getFullYear() + 1;
+        date.setFullYear(updatedYear);
+        return date;
+    }
+  }
+})
