3b3586377ad3ab03fae777dbbe6c3d024e3468ca
Beth Smith <bethsmith0623@gamil.com>
2019-08-16 09:55:56 -0500

    adds config code to mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb//localhost:27017/mongooseFlights', {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+
+const db = mongoose.connection;
+
+db.on('connected', function(){
+  console.log(`${db.name} is connected on ${db.host}`);
+});
+
+db.on('error', function(err){
+  console.log(`MongoDB has thrown an error: `, err);
+});
