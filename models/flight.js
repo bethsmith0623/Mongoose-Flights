626bd48cd8cd1f6f283a4befd2139dccd68fd786
Beth Smith <bethsmith0623@gamil.com>
2019-08-18 21:36:05 -0500

    updates models

--- models/flight.js
+++ models/flight.js
@@ -1,24 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const destinationSchema = new Schema({
-  airport: {
-    type: String,
-    enum: ['AUS', 'DAL', 'LAX', 'SEA']
-  },
-  arrival: {
-    type: Date, 
-    default: function(){
-        // 08/16/2019 22:20 CDT convert to 08/19/2020 22:20 CDT
-        const date = new Date();
-        const updatedYear = date.getFullYear() + 1;
-        date.setFullYear(updatedYear);
-        return date + 1;
-      }
-    }, 
-  }, { timestamps: true });
-
-
 const flightSchema = new Schema({
   airline: {
     type: String,
