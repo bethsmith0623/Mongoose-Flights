a7e56420f82336263897e558f3918ada068ae780
Beth Smith <bethsmith0623@gamil.com>
2019-08-19 10:21:16 -0500

    fixes minor display errors

--- models/flight.js
+++ models/flight.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const destinationSchema = require('./destination');
 
 const flightSchema = new Schema({
   airline: {
@@ -27,9 +28,8 @@ const flightSchema = new Schema({
     enum: ['SEA', 'DAL', 'LAX', 'AUS'],
     default: 'SEA'
   },
-  // destinations: {
-  //   type: [destinationSchema]
-  // },
+  destinations: [{type: Schema.Types.ObjectId, ref: 'Destination'}]
+  ,
 }, { timestamps: true });
 
 module.exports = mongoose.model('Flight', flightSchema);
