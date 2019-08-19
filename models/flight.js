46b2dc37b0dfde80da51995aaabb3d25f59f0ca2
Beth Smith <bethsmith0623@gamil.com>
2019-08-18 20:38:37 -0500

    adds destination schema

--- models/flight.js
+++ models/flight.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+  airportArr: {
+    type: String,
+    enum: ['AUS', 'DAL', 'LAX', 'SEA']
+  },
+  arrival: {
+    type: Date, 
+    default: function(){
+        // 08/16/2019 22:20 CDT convert to 08/19/2020 22:20 CDT
+        const date = new Date();
+        const updatedYear = date.getFullYear() + 1;
+        date.setFullYear(updatedYear);
+        return date + 1;
+      }
+    }, }, { timestamps: true });
+
 const flightSchema = new Schema({
   airline: {
     type: String,
@@ -26,9 +42,8 @@ const flightSchema = new Schema({
     type: String,
     enum: ['AUS', 'DAL', 'LAX', 'SEA']
   },
-  airportArr: {
-    type: String,
-    enum: ['AUS', 'DAL', 'LAX', 'SEA']
+  destinations: {
+    type: [destinationSchema]
   },
 }, { timestamps: true });
 
